1ea0b5ed96451d61d7d29dc37818e2877b085ee8
ChopinNocture <chopin_7up@icloud.com>
2017-05-18 03:52:12 +0800

    第一层

--- Chapter-3/Hamming.py
+++ Chapter-3/Hamming.py
@@ -10,8 +10,23 @@ Created on Thu May 18 01:13:49 2017
 @author: 7up
 """
 
-x=""
-input(x)
-print("先学numPy" + x)
+import numpy as nplib
+
+#输入
+p=nplib.array([-1,-1,-1])
+
+
+#前馈
+#权值
+W1=nplib.array([[1,-1,-1],[1,1,-1]])
+#偏置
+b1=nplib.array([3,3])
+
+a1=W1.dot(p)+b1
+           
+#          
+           
+print(W1.dot(p))
+print(a1)
 
 
