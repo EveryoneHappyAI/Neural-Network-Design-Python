ffeaffd33077a3b8d57bb00bc319b3200b0e3e92
ChopinNocture <chopin_7up@icloud.com>
2017-05-18 01:22:54 +0800

    建立项目
    建立项目，先学python3 以及numpy

---
+++ Chapter-3/Hamming.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+神经网络设计
+3.2.3 Hamming网络
+purelin函数作为前馈层，poslin函数作为递归（反馈）层
+专门为求解二值模式识别问题设计
+
+Created on Thu May 18 01:13:49 2017
+
+@author: 7up
+"""
+
+x=""
+input(x)
+print("先学numPy" + x)
+
+
